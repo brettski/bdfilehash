56308bb17563181188884606bd59a18e65853295
brettski <brettski@localhost>
2014-12-10 23:15:44 -0600

    Saving Chocolatey configs

--- trunk/chocolatey/bdfilehash/tools/chocolateyInstall.ps1
+++ trunk/chocolatey/bdfilehash/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'bdfilehash'
 $installerType = 'EXE'
-$url = 'http://bdfilehash.codeplex.com/downloads/get/250123'
+$url = 'https://bdfilehash.codeplex.com/downloads/get/977851'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
