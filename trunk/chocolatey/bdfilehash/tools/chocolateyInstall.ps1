0bee66d484f11c82e43ef96a310cd1661e4716d5
Brett Slaski <brettski@yahoo.com>
2018-07-21 23:09:25 -0500

    Update newspec for v1.1.4. new github references

--- trunk/chocolatey/bdfilehash/tools/chocolateyInstall.ps1
+++ trunk/chocolatey/bdfilehash/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'bdfilehash'
 $installerType = 'EXE'
-$url = 'https://bdfilehash.codeplex.com/downloads/get/977851'
+$url = 'https://github.com/brettski/bdfilehash/releases/download/v1.1.4/BDFileHashSetup.1.1.4.exe'
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
