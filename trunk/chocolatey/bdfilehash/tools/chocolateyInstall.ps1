3a3a958ac40a6e1a8c0130eb4413274c03047f23
Brett Slaski <brettski@yahoo.com>
2018-07-28 19:56:30 -0500

    Fix issues for Chocolatey

--- trunk/chocolatey/bdfilehash/tools/chocolateyInstall.ps1
+++ trunk/chocolatey/bdfilehash/tools/chocolateyInstall.ps1
@@ -1,6 +1,13 @@
-$packageName = 'bdfilehash'
-$installerType = 'EXE'
+
 $url = 'https://github.com/brettski/bdfilehash/releases/download/v1.1.4/BDFileHashSetup.1.1.4.exe'
-$silentArgs = '/S' 
-$validExitCodes = @(0) 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$SHA256='aec7742b6b402ed1ac31d75c8ea91cadc425333a9baed0ed7d244af2146b3209'
+$chocoInstArgs = @{
+    PackageName = 'bdfilehash'
+    InstallerType = 'EXE' 
+    silentArgs = '/S' 
+    url = "$url" 
+    validExitCodes = @(0) 
+    checkSum = $SHA256 
+    checksumType = "SHA256"
+}
+Install-ChocolateyPackage $chocoInstArgs
--- trunk/chocolatey/bdfilehash/tools/chocolateyUninstall.ps1
+++ trunk/chocolatey/bdfilehash/tools/chocolateyUninstall.ps1
@@ -2,7 +2,6 @@ $packageName = 'bdfilehash';
 $installerType = 'exe';
 $silentArgs = '/S';
  
-try {
   $hklm = "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\BD File Hash";
   if (Get-ProcessorBits 64) {
       $hklm = "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\BD File Hash";
@@ -10,8 +9,3 @@ try {
   $file = (Get-ItemProperty -Path $hklm).UninstallString;
   Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -silentArgs $silentArgs -File $file;
    
-  Write-ChocolateySuccess $packageName;
-} catch {
-  Write-ChocolateyFailure $packageName "$($_.Exception.Message)";
-  throw;
-}
